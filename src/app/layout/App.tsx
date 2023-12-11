4008d60269b340be4a4dde45481eb9d353840476
Szabolcs Peter <szabi.peter@gmail.com>
2023-12-11 09:38:16 +0100

    adding AppBar

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Typography } from "@mui/material";
+import Header from "./Header";
+import { Container, CssBaseline } from "@mui/material";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -25,10 +26,13 @@ function App() {
   }
 
   return (
-    <div>
-      <Typography variant="h1">Skinet</Typography>
-      <Catalog products={products} addProduct={addProduct}/>
-    </div>
+    <>
+    <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog products={products} addProduct={addProduct}/>
+      </Container>
+    </>
   )
 }
 
